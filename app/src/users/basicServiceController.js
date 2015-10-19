9675e4d92e3de3cd260e16b62f9cc2ee7bcaca8e
UVBrain services <uvbrainsolutions@outlook.com>
2015-10-19 11:07:41 +0530

    removed unused controller

--- app/src/users/basicServiceController.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-angular.module('users').controller('ChapterController',
-	function ($scope, $routeParams) {
-		$scope.name = "ChapterController";
-		$scope.params = $routeParams;
-	});
